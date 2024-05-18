8c2a2ae33e19f9a73093a83527d563ef514e00e0
joaoalfredo <joaoalfredo229@gmail.com>
2024-05-18 15:38:19 -0300

    first commit

---
+++ src/controller/Paciente.java
@@ -0,0 +1,11 @@
+package controller;
+import java.util.ArrayList;
+
+
+public class Paciente {
+    String nome, dietaAlimetar;
+    int idade;
+    double peso, altura, pressaoArterial, frequenciaCardiaca;
+    ArrayList<String> atividadesFisicas = new ArrayList<>();
+
+}
